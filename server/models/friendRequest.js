0d547da2a354ff6c91693472605c713a0eec5f3c
Gagandeep Bhardwaj <gbhardwaj572@gmail.com>
2024-03-19 21:24:38 +0530

    authentication done

---
+++ server/models/friendRequest.js
@@ -0,0 +1,11 @@
+import mongoose , {Schema} from mongoose;
+const friendRequestSchema = Schema({
+    requestTo : { type:Schema.Types.ObjectId, ref: "Users"},
+    requestFrom : { type:Schema.Types.ObjectId, ref: "Users"},
+    requestStatus: { type: String, default: "Pending" },
+  },
+  { timestamps: true }
+);
+
+const FriendRequest = mongoose.model("FriendRequest", requestSchema);
+export default FriendRequest;
